830b676234099b7ca606e1e2e039a07d23f899ac
Jefferson <pasante2@tramasec.com>
2018-11-24 12:25:23 -0500

    Estructura Proyecto

--- deber_arquitectura_software/src/main/java/ec/edu/espe/arquitectura/modelo/CiudadanoMongo.java
+++ deber_arquitectura_software/src/main/java/ec/edu/espe/arquitectura/modelo/CiudadanoMongo.java
@@ -5,10 +5,104 @@
  */
 package ec.edu.espe.arquitectura.modelo;
 
+import java.util.Date;
+import java.util.Objects;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.IndexOptions;
+import org.mongodb.morphia.annotations.Indexed;
+import org.mongodb.morphia.annotations.Property;
+
 /**
  *
  * @author jefferson
  */
+@Entity(noClassnameStored = true,value="usuario")
 public class CiudadanoMongo {
+    @Id
+    private ObjectId id;
     
+    @Property("cedula")
+    @Indexed(options=@IndexOptions(name="idxCodCiud"),unique = true)
+    private String cedula;
+    
+    @Property("nombre")
+    private String nombre;
+    
+    @Property("apellido")
+    private String apellido;
+    
+    @Property("fechaNacimiento")
+    private Date fechaNacimiento;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.cedula);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CiudadanoMongo other = (CiudadanoMongo) obj;
+        if (!Objects.equals(this.cedula, other.cedula)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Ciudadano{" + "id=" + id + ", cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + ", fechaNacimiento=" + fechaNacimiento + '}';
+    }
 }
